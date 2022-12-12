9336a7ca3658511ce5fad5dcf379d811f9e848b0
FloatinggOnion <jesseosems123@gmail.com>
2022-12-12 03:53:34 -0800

    C file using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void){
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
