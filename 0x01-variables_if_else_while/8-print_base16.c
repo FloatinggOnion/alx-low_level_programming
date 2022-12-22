6ce3c17e827aec8829924dd7262952e0d1ed942a
FloatinggOnion <jesseosems123@gmail.com>
2022-12-22 02:10:56 -0800

    Print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[6] = "abcdef";
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (i=0; i < 6; i++)
+		putchar(alphabet[i]);
+
+	putchar('\n');
+	return (0);
+}
